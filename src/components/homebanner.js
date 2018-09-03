1e34c3eb5aa880d456b8866cdc285ef2f308f2eb
pngo12 <pngo12@gmail.com>
2018-09-03 12:56:26 -0700

    installed bulma globally

--- src/components/homebanner.js
+++ src/components/homebanner.js
@@ -7,19 +7,19 @@ const HomeBanner = () => {
     return ( 
         
         <div className="columns is-gapless">
-            <div className="column is-4 has-background-primary is-narrow">
+            <div className="column is-4 has-background-grey is-narrow">
                 <div className="container is-fluid">
                     <br />
                     <figure className="image is-128x128">
                         <img src={shocked} />
                     </figure>
                     <h1 className="title">Options got your head exploding?</h1>
-                    <p classNameName="content is-pulled-left">
+                    <p classNameName="content is-pulled-left has-text-white">
                         Don't worry, we know that feeling. 5000 channels yet "nothing" to watch. Too
                                 many options leave you overwhelemd. We do the choosing, and you do the eating.</p>
                 </div>
             </div>
-            <div className="column is-4 has-background-link is-narrow">
+            <div className="column is-4 has-background-grey-dark is-narrow">
                 <div className="container is-fluid">
                     <br />
                     <figure className="image is-128x128 is-inline-block">
@@ -29,12 +29,12 @@ const HomeBanner = () => {
                         <img src={girlNo} />
                     </figure>
                     <h1 className="title">Indecisive group?</h1>
-                    <p className="content is-pulled-left">If your group of friends are the indecisive type then we're the solution type.
+                    <p className="content is-pulled-left has-text-white">If your group of friends are the indecisive type then we're the solution type.
                  LUC isn't just for your boo, it's for the homies too. Let us know what your options are, and we'll randomly choose one.</p>
                 </div>
 
             </div>
-            <div className="column is-4 has-background-warning is-narrow">
+            <div className="column is-4 has-background-grey-light is-narrow">
 
                 <div className="container is-fluid">
                     <br />
@@ -42,7 +42,7 @@ const HomeBanner = () => {
                         <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/apple/129/right-pointing-magnifying-glass_1f50e.png" />
                     </figure>
                     <h1 className="title">Options got your head exploding?</h1>
-                    <p className="content is-pulled-left">Don't worry, we know that feeling. 5000 channels yet there's "nothing" to watch. Too
+                    <p className="content is-pulled-left has-text-white">Don't worry, we know that feeling. 5000 channels yet there's "nothing" to watch. Too
                     many options leave you overwhelemd. So let us do the choosing, and you do the eating.</p>
                 </div>
 
