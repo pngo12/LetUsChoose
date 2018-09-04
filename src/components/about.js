daccedf40c9c5cb11f6cf756f32d9837b4c09c45
Kevin Moon <kevin.moon421@gmail.com>
2018-09-04 10:20:06 -0700

    fixed about styling

--- src/components/about.js
+++ src/components/about.js
@@ -5,20 +5,19 @@ import john from '../images/john.jpg'
 
 const About = () => {
     return ( 
-            <section className="section is-medium">
+            <section className="section is-small">
             <div className="container">
+                <img src='https://images.pexels.com/photos/323705/pexels-photo-323705.jpeg?auto=compress&cs=tinysrgb&h=350' />
+                </div>
+            <div className="container about">
                 <div className="columns">
                     <div className="column">
-                        <p><strong>About LUC</strong></p>
-                    </div>
-                    <div className="column">
-                    </div>
-                    <div className="column">
+                        <p className="title"><strong>About LUC</strong></p>
                     </div>
                 </div>
                 <div className="columns">
                     <div className="column is-8">
-                        <p className="text-justify">
+                        <p className="text-justify subtitle">
                             Are you tired of your boo asking you where to eat? Tired of them taking hours just
                             to choose a restaurant? Well, LUC aims to solve that problem. Just let us know a few things about
                             your cravings, and we'll give you a random restauarnt based on your criteria. Now instead of taking
