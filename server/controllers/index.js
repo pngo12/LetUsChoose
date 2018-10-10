37fa111277ac61a42ac36fa9b1e826a096ee1325
William Yang <williamyang93@gmail.com>
2018-10-10 00:46:37 -0700

    Updated findRestaurants controller to have GraphQL query with variable parameter. Renamed query to findRestaurantsQuery, updated anonymous query to findRestaurantsQuery and passed in $alias and $location as parameters into the query Reformatted GraphQL Query

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,43 +2,37 @@ const yelp = require('yelp-fusion')
 const key = require('../key')
 const { GraphQLClient } = require('graphql-request');
 
-const findRestaurants = async (req, res,) => {
-    let {alias, location} = req.params;
+const findRestaurants = async (req, res) => {
+    let { alias, location } = req.params;
     try {
-        const query = `query 
-            {
-                search (
-                    term: ${alias}, 
-                    location: ${location},
-                    radius: 2500
-                    )
-            {
-            business { 
-            name 
-            price
-            hours {
-              is_open_now
-              open {
-                start
-                end
-              }
-            }
-            display_phone
-            location {
-              formatted_address
-            }
-        }
-    }
-}`
+        const findRestaurantsQuery = `
+            query findRestaurantsQuery($alias: String, $location: String) {
+                search (term: $alias, location: $location, radius: 2500) {
+                    business {
+                        name
+                        price
+                        hours {
+                            is_open_now
+                            open {
+                                start
+                                end
+                            }       
+                        }
+                        display_phone
+                        location {
+                            formatted_address
+                        }
+                    }
+                }
+            }`
         const endpoint = "https://api.yelp.com/v3/graphql";
         const graphQLClient = new GraphQLClient(endpoint, {
             headers: {
                 'Authorization': `Bearer ${key}`
             }
         })
-        const data = await graphQLClient.request(query);
+        const data = await graphQLClient.request(findRestaurantsQuery, { location, alias });
         let yelpResult = await data.search.business[Math.floor(Math.random() * 20)]
-    
         res.status(200).send(yelpResult)
     }
     catch (e) {
